00dc8097dcb4ca12b9475d247c0a50b8970fa182
joakim <joakim.cedergren1@gmail.com>
2021-03-14 14:09:55 +0100

    Code formatting - following linux kernel style

--- bmp280-spi.c
+++ bmp280-spi.c
@@ -11,66 +11,74 @@ static dev_t device_nbr;
 static struct class *device_class;
 static struct cdev c_device;
 
-static int bmp280_spi_open(struct inode *pinode, struct file *pfile){
-    printk(KERN_NOTICE "BMP280_spi opened\n");
-    return 0;
+static int bmp280_spi_open(struct inode *pinode, struct file *pfile)
+{
+	printk(KERN_NOTICE "BMP280_spi opened\n");
+	return 0;
 }
 
-static int bmp280_spi_close(struct inode *inode, struct file *pfile){
-    printk(KERN_NOTICE "BMP280_spi closed\n");
-    return 0;
+static int bmp280_spi_close(struct inode *inode, struct file *pfile)
+{
+	printk(KERN_NOTICE "BMP280_spi closed\n");
+	return 0;
 }
 
 static struct file_operations bmp280_spi_fops = {
-    .owner = THIS_MODULE,
-    .open = bmp280_spi_open,
-    .release = bmp280_spi_close,
+	.owner = THIS_MODULE,
+	.open = bmp280_spi_open,
+	.release = bmp280_spi_close,
 };
 
-static int bmp280_spi_init(void){
-    printk(KERN_INFO "%s - Initialization started.\n", DRIVER_NAME);
-    
-    if(alloc_chrdev_region(&device_nbr, 0, 1, DRIVER_NAME) < 0) {
-        printk("%s - Device nbr could not be allocated.\n", DRIVER_NAME);
-        return -1;
-    }
-    printk("%s - Device nbr. registered, Major: %d, Minor: %d.\n", DRIVER_NAME, device_nbr >> 20, device_nbr && 0xfffff);
-        
-    if((device_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL){
-        printk("%s - Device class cannot be created\n", DRIVER_NAME);
-        goto ClassError;  
-    }
+static int bmp280_spi_init(void)
+{
+	printk(KERN_INFO "%s - Initialization started.\n", DRIVER_NAME);
 
-    if(device_create(device_class, NULL, device_nbr, NULL, DRIVER_NAME) == NULL){
-        printk("%s - Cannot create device file.\n", DRIVER_NAME);
-        goto FileError;
-    }
-    cdev_init(&c_device, &bmp280_spi_fops);
+	if (alloc_chrdev_region(&device_nbr, 0, 1, DRIVER_NAME) < 0) {
+		printk("%s - Device nbr could not be allocated.\n",
+		       DRIVER_NAME);
+		return -1;
+	}
+	printk("%s - Device nbr. registered, Major: %d, Minor: %d.\n",
+	       DRIVER_NAME, device_nbr >> 20, device_nbr && 0xfffff);
 
-    /* Registering device to kernel */
-    if(cdev_add(&c_device, device_nbr, 1) == -1){
-        printk("%s - Registering of device to kernel failed.\n", DRIVER_NAME);
-        goto AddError;
-    }
-    printk(KERN_INFO "%s - Initialization complete.\n", DRIVER_NAME);
+	if ((device_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+		printk("%s - Device class cannot be created\n", DRIVER_NAME);
+		goto ClassError;
+	}
 
-    return 0;
+	if (device_create(device_class, NULL, device_nbr, NULL, DRIVER_NAME) ==
+	    NULL) {
+		printk("%s - Cannot create device file.\n", DRIVER_NAME);
+		goto FileError;
+	}
+	cdev_init(&c_device, &bmp280_spi_fops);
+
+	/* Registering device to kernel */
+	if (cdev_add(&c_device, device_nbr, 1) == -1) {
+		printk("%s - Registering of device to kernel failed.\n",
+		       DRIVER_NAME);
+		goto AddError;
+	}
+	printk(KERN_INFO "%s - Initialization complete.\n", DRIVER_NAME);
+
+	return 0;
 
 AddError:
-    device_destroy(device_class, device_nbr);
+	device_destroy(device_class, device_nbr);
 FileError:
-    class_destroy(device_class);
+	class_destroy(device_class);
 ClassError:
-    unregister_chrdev_region(device_nbr, 1);
-    return -1;   
+	unregister_chrdev_region(device_nbr, 1);
+	return -1;
 }
 
-static void bmp280_spi_exit(void){
-    cdev_del(&c_device);
-    device_destroy(device_class, device_nbr);
-    class_destroy(device_class);
-    unregister_chrdev_region(device_nbr, 1);
-    printk(KERN_INFO "%s - Unregistered.\n", DRIVER_NAME);
+static void bmp280_spi_exit(void)
+{
+	cdev_del(&c_device);
+	device_destroy(device_class, device_nbr);
+	class_destroy(device_class);
+	unregister_chrdev_region(device_nbr, 1);
+	printk(KERN_INFO "%s - Unregistered.\n", DRIVER_NAME);
 }
 
 MODULE_LICENSE("GPL");
